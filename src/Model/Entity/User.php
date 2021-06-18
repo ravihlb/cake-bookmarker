13bfa731b8cfed0014eabb981643406c8ba4c67e
ravihlb <ravi@deliverydireto.com.br>
2021-06-18 15:39:52 -0300

    Add password hashing

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -43,4 +44,9 @@ class User extends Entity
     protected $_hidden = [
         'password',
     ];
+
+    protected function _setPassword($value) {
+        $hasher = new DefaultPasswordHasher();
+        return $hasher->hash($value);
+    }
 }
