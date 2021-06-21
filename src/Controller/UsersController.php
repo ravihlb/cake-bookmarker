036f28d480a712c3d059138fe97de864b3b40cd7
ravihlb <ravi@deliverydireto.com.br>
2021-06-21 13:58:51 -0300

    Enable registrations

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -25,7 +25,7 @@ class UsersController extends AppController
 
     public function initialize(): void {
         parent::initialize();
-        $this->Auth->allow(['logout']);
+        $this->Auth->allow(['logout', 'add']);
     }
 
     /**
